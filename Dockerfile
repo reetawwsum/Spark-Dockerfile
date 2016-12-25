ae1192c4aa6ba55054446be5d787ff4957db3048
Reet Awwsum <reetawwsum@yahoo.com>
2016-12-25 16:46:00 +0530

    Added spark dockerfile with dependencies

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM reetawwsum/hadoop
+
+MAINTAINER Reet Awwsum <reetawwsum@yahoo.com>
+
+RUN curl http://d3kbcqa49mib13.cloudfront.net/spark-2.0.2-bin-hadoop2.7.tgz | tar -xz -C /usr/local
+RUN ln -s /usr/local/spark-2.0.2-bin-hadoop2.7 /usr/local/spark
+
+ENV SPARK_HOME /usr/local/spark
+
+RUN mkdir $SPARK_HOME/yarn-remote-client
+ADD yarn-remote-client $SPARK_HOME/yarn-remote-client
+
+ENV PATH $PATH:$SPARK_HOME/bin:$HADOOP_PREFIX/bin
+
+ADD bootstrap.sh /usr/local/src/bootstrap.sh
+RUN chown root:root /usr/local/src/bootstrap.sh
+RUN chmod 700 /usr/local/src/bootstrap.sh
+
+ENTRYPOINT ["/usr/local/src/bootstrap.sh"]
---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+: ${HADOOP_PREFIX:=/usr/local/hadoop}
+
+rm /tmp/*.pid
+
+cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
+
+sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
+sed -i s/HOSTNAME/$HOSTNAME/ $SPARK_HOME/yarn-remote-client/core-site.xml
+sed -i s/HOSTNAME/$HOSTNAME/ $SPARK_HOME/yarn-remote-client/yarn-site.xml
+
+cp $SPARK_HOME/conf/metrics.properties.template $SPARK_HOME/conf/metrics.properties
+
+/usr/sbin/sshd
+$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
+$HADOOP_PREFIX/sbin/start-dfs.sh
+$HADOOP_PREFIX/sbin/start-yarn.sh
+
+/bin/bash
